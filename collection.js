e8d7f265695c12c52a62f37970498cb9ea4cedc6
n4zr-sketcher <nazeer.game07@gmail.com>
2026-09-13 02:08:05 +0530

    added responsive design and side nav bar for mob view

--- collection.js
+++ collection.js
@@ -33,14 +33,18 @@ searchInput.addEventListener("keyup", function () {
 });
 
 
-var sidenav = document.getElementById("sidenav")
-var closenav = document.getElementById("closenav")
-var menuicon = document.getElementById("menuicon")
+var sidenav = document.getElementById("sidenav");
+var closenav = document.getElementById("closenav");
+var menuicon = document.getElementById("menu-icon") || document.getElementById("menuicon");
 
-menuicon.addEventListener("click", function(){
-    sidenav.style.right = "0"
-})
+if (menuicon && sidenav) {
+  menuicon.addEventListener("click", function () {
+    sidenav.style.right = "0";
+  });
+}
 
-closenav.addEventListener("click", function(){
-    sidenav.style.right = "-50%"
-})
+if (closenav && sidenav) {
+  closenav.addEventListener("click", function () {
+    sidenav.style.right = "-100%";
+  });
+}
--- script.js
+++ script.js
@@ -1,64 +1,71 @@
 var notifyBar = document.getElementById("notifyBar");
 var closeButton = document.getElementById("closeButton");
 
-closeButton.addEventListener("click", function () {
-  notifyBar.style.display = "none";
-});
+if (closeButton && notifyBar) {
+  closeButton.addEventListener("click", function () {
+    notifyBar.style.display = "none";
+  });
+}
 
 var contactName = document.getElementById("contactName");
 var nameError = document.getElementById("nameError");
 
-contactName.addEventListener("input", function () {
-  if (contactName.value == "") {
-    nameError.style.display = "block";
-  } else {
-    nameError.style.display = "none";
-  }
-});
+if (contactName && nameError) {
+  contactName.addEventListener("input", function () {
+    if (contactName.value == "") {
+      nameError.style.display = "block";
+    } else {
+      nameError.style.display = "none";
+    }
+  });
+}
 
 var contactEmail = document.getElementById("contactEmail");
 var emailError = document.getElementById("emailError");
 var regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$/;
 
-contactEmail.addEventListener("input", function () {
-  if (contactEmail.value == "") {
-    emailError.stlye.display = "block";
-  } else {
-    emailError.style.display = "none";
-  }
-  if (!regex.test(contactEmail.value)) {
-    emailError.style.display = "block";
-  } else {
-    emailError.style.display = "none";
-  }
-  if (contactEmail.value == "") emailError.style.display = "none";
-});
+if (contactEmail && emailError) {
+  contactEmail.addEventListener("input", function () {
+    if (contactEmail.value == "") {
+      emailError.style.display = "block";
+    } else {
+      emailError.style.display = "none";
+    }
+    if (!regex.test(contactEmail.value)) {
+      emailError.style.display = "block";
+    } else {
+      emailError.style.display = "none";
+    }
+    if (contactEmail.value == "") emailError.style.display = "none";
+  });
+}
 
 var contactSubject = document.getElementById("contactSubject");
 var subjectError = document.getElementById("subjectError");
 
-contactSubject.addEventListener("input", function () {
-  if (contactSubject.value == "") {
-    subjectError.style.display = "block";
-  } else {
-    subjectError.style.display = "none";
-  }
-});
+if (contactSubject && subjectError) {
+  contactSubject.addEventListener("input", function () {
+    if (contactSubject.value == "") {
+      subjectError.style.display = "block";
+    } else {
+      subjectError.style.display = "none";
+    }
+  });
+}
 
-var closeButton = document.getElementById("closeButton");
-var notifyBar = document.getElementById("notifyBar");
-closeButton.addEventListener("click", function () {
-  notifyBar.style.display = "none";
-});
+var sidenav = document.getElementById("sidenav");
+var closenav = document.getElementById("closenav");
+var menuicon = document.getElementById("menu-icon") || document.getElementById("menuicon");
 
-var sidenav = document.getElementById("sidenav")
-var closenav = document.getElementById("closenav")
-var menuicon = document.getElementById("menuicon")
+if (menuicon && sidenav) {
+  menuicon.addEventListener("click", function () {
+    sidenav.style.right = "0";
+  });
+}
 
-menuicon.addEventListener("click", function(){
-    sidenav.style.right = "0"
-})
+if (closenav && sidenav) {
+  closenav.addEventListener("click", function () {
+    sidenav.style.right = "-100%";
+  });
+}
 
-closenav.addEventListener("click", function(){
-    sidenav.style.right = "-50%"
-})
