4decbe6477bb88a166a70b592b6ccaaded3dfb5e
n4zr-sketcher <nazeer.game07@gmail.com>
2026-09-19 16:06:23 +0530

    added filters in collection page with checkbox

--- collection.js
+++ collection.js
@@ -27,22 +27,58 @@ if (contactName && nameError) {
 var searchInput = document.getElementById("searchInput");
 var products = document.querySelectorAll(".new-arrivals_item");
 var noresult = document.getElementById("noResult");
+var categoryFilters = document.querySelectorAll(".category-filter");
+var priceFilters = document.querySelectorAll(".price-filter");
+var clearFilters = document.getElementById("clearFilters");
 
 console.log("Search input:", searchInput);
 console.log("Products:", products.length);
 
-searchInput.addEventListener("keyup", function () {
+function showProducts() {
   var enteredvalue = searchInput.value.toUpperCase();
+  var selectedCategories = [];
+  var selectedPrices = [];
   var matchfound = false;
 
+  categoryFilters.forEach(function (filter) {
+    if (filter.checked) {
+      selectedCategories.push(filter.value);
+    }
+  });
+
+  priceFilters.forEach(function (filter) {
+    if (filter.checked) {
+      selectedPrices.push(filter.value);
+    }
+  });
+
   products.forEach(function (product) {
-    var productName = product
-      .querySelector(".item-srch")
-      .textContent.toUpperCase();
+    var productName = product.querySelector(".item-srch").textContent.toUpperCase();
+    var priceText = product.querySelector(".item-srch").nextElementSibling.textContent;
+    var productPrice = parseInt(priceText.match(/\d+/)[0]);
+    var categoryMatched = selectedCategories.length == 0;
+    var priceMatched = selectedPrices.length == 0;
 
-    if (productName.includes(enteredvalue)) {
+    selectedCategories.forEach(function (category) {
+      if (productName.toLowerCase().includes(category)) {
+        categoryMatched = true;
+      }
+    });
+
+    selectedPrices.forEach(function (price) {
+      if (price == "under-500" && productPrice < 500) {
+        priceMatched = true;
+      }
+      if (price == "500-1500" && productPrice >= 500 && productPrice <= 1500) {
+        priceMatched = true;
+      }
+      if (price == "over-1500" && productPrice > 1500) {
+        priceMatched = true;
+      }
+    });
+
+    if (productName.includes(enteredvalue) && categoryMatched && priceMatched) {
       product.style.display = "";
-
       matchfound = true;
     } else {
       product.style.display = "none";
@@ -54,6 +90,29 @@ searchInput.addEventListener("keyup", function () {
   } else {
     noresult.style.display = "block";
   }
+}
+
+searchInput.addEventListener("keyup", showProducts);
+
+categoryFilters.forEach(function (filter) {
+  filter.addEventListener("change", showProducts);
+});
+
+priceFilters.forEach(function (filter) {
+  filter.addEventListener("change", showProducts);
+});
+
+clearFilters.addEventListener("click", function () {
+  categoryFilters.forEach(function (filter) {
+    filter.checked = false;
+  });
+
+  priceFilters.forEach(function (filter) {
+    filter.checked = false;
+  });
+
+  searchInput.value = "";
+  showProducts();
 });
 
 
